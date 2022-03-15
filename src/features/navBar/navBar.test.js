33cbf4102dde6723b6e5d824af892e737968127c
drattylight <mlaux6@outlook.com>
2022-03-14 21:55:11 -0500

    Add router to navBar tests and refactor tests

--- src/features/navBar/navBar.test.js
+++ src/features/navBar/navBar.test.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NavBar from './navBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 describe('<NavBar />', () => {
 
   test('logo renders', () => {
-    render(<NavBar />);
+    render(
+      <Router>
+        <Routes>
+          <Route path='*' element={<NavBar />} />
+        </Routes>
+      </Router>
+    );
     const logo = screen.getByRole('img', {name: /reddit logo/i});
     const redditTitle = screen.getByRole('heading', { name: /reddit/i});
     
@@ -14,23 +21,33 @@ describe('<NavBar />', () => {
     expect(redditTitle).toBeInTheDocument();
   });
 
-  test('home link sends user back to main screen', () => {
-    render(<NavBar />);
-    const homeButton = screen.getByRole('navigation', { name: /home/i});
+  test('home is a functioning link', () => {
+    render(
+      <Router>
+        <Routes>
+          <Route path='*' element={<NavBar />} />
+        </Routes>
+      </Router>
+    );
+    const homeButton = screen.getByRole('link', { name: /home/i});
 
     expect(homeButton).toBeInTheDocument();
   });
 
-  test('search Reddit bar initializes with Search Reddit, clears on click, and accepts typed input', () => {
-    render(<NavBar />);
-    const searchBar = screen.getByRole('search', { name: /search reddit/i});
+  test('search Reddit bar initializes with placeholder Search Reddit and accepts typed input', () => {
+    render(
+      <Router>
+        <Routes>
+          <Route path='*' element={<NavBar />} />
+        </Routes>
+      </Router>
+    );
+    const searchBar = screen.getByPlaceholderText(/search reddit/i);
 
     expect(searchBar).toBeInTheDocument();
-    expect(searchBar).toHaveValue(/search reddit/i);
     userEvent.click(searchBar);
-    expect(searchBar).toBeEmptyDOMElement();
-    userEvent.type('Test input for reddit search bar');
-    expect(searchBar).toHaveValue(/test input for reddit search bar/i);
+    userEvent.type(searchBar, 'Test input for reddit search bar');
+    expect(searchBar).toHaveValue('Test input for reddit search bar');
   });
 
 });
