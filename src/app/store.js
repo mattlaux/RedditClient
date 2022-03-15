766f0a23229896e4b9231eeeceb6f90363287237
drattylight <mlaux6@outlook.com>
2022-03-15 14:40:04 -0500

    Add sortPosts reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import sortPostsReducer from '../features/sortPosts/sortPostsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    sortPosts: sortPostsReducer
   },
 });
