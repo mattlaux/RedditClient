34db61118cae5f3aea79b6360eff801343e0389c
drattylight <mlaux6@outlook.com>
2022-03-18 09:25:25 -0500

    Refactor action and function names to match reducer. Remove addPosts test

--- src/features/posts/postsSlice.test.js
+++ src/features/posts/postsSlice.test.js
@@ -1,6 +1,6 @@
-import sortPostsReducer, { addPosts, changeSortCategory, removePosts, sortPostsAsync } from './postsSlice';
+import postsReducer, { changeSortCategory, removePosts, fetchPosts } from './postsSlice';
 
-describe('sort posts reducer', () => {
+describe('posts reducer', () => {
   const initialState = {
     sortCategory: 'hot',
     error: null,
@@ -8,7 +8,7 @@ describe('sort posts reducer', () => {
   };
 
   test('should handle initial state', () => {
-    expect(sortPostsReducer(undefined, { type: 'unknown' })).toEqual({
+    expect(postsReducer(undefined, { type: 'unknown' })).toEqual({
       sortCategory: 'hot',
       error: null,
       status: 'idle',
@@ -17,256 +17,27 @@ describe('sort posts reducer', () => {
   });
 
   test('should handle changeSortCategory', () => {
-    const actual = sortPostsReducer(initialState, changeSortCategory('new'));
+    const actual = postsReducer(initialState, changeSortCategory('new'));
     expect(actual.sortCategory).toEqual('new');
   });
 
   test('should handle removePosts', () => {
-    const actual = sortPostsReducer(initialState, removePosts());
+    const actual = postsReducer(initialState, removePosts());
     expect(actual.posts).toEqual([]);
   });
 
-  test('should handle addPosts', () => {
-    const actual = sortPostsReducer(initialState, addPosts(
-      {
-        approved_at_utc: null,
-        subreddit: 'greentext',
-        selftext: '',
-        author_fullname: 't2_edyu9t3r',
-        saved: false,
-        mod_reason_title: null,
-        gilded: 0,
-        clicked: false,
-        title: 'Anon is a rapist',
-        link_flair_richtext: [],
-        subreddit_name_prefixed: 'r/greentext',
-        hidden: false,
-        pwls: 0,
-        link_flair_css_class: null,
-        downs: 0,
-        thumbnail_height: 140,
-        top_awarded_type: null,
-        hide_score: false,
-        name: 't3_tep5ud',
-        quarantine: false,
-        link_flair_text_color: 'dark',
-        upvote_ratio: 0.95,
-        author_flair_background_color: null,
-        subreddit_type: 'public',
-        ups: 18578,
-        total_awards_received: 4,
-        media_embed: {},
-        thumbnail_width: 140,
-        author_flair_template_id: null,
-        is_original_content: false,
-        user_reports: [],
-        secure_media: null,
-        is_reddit_media_domain: true,
-        is_meta: false,
-        category: null,
-        secure_media_embed: {},
-        link_flair_text: null,
-        can_mod_post: false,
-        score: 18578,
-        approved_by: null,
-        is_created_from_ads_ui: false,
-        author_premium: false,
-        thumbnail: 'https://b.thumbs.redditmedia.com/WuafJXjS4QF29kz7W2yelt380P1qs8SegVzmYUPd8hM.jpg',
-        edited: false,
-        author_flair_css_class: null,
-        author_flair_richtext: [],
-        gildings: [Object],
-        post_hint: 'image',
-        content_categories: null,
-        is_self: false,
-        mod_note: null,
-        created: 1647351356,
-        link_flair_type: 'text',
-        wls: 0,
-        removed_by_category: null,
-        banned_by: null,
-        author_flair_type: 'text',
-        domain: 'i.redd.it',
-        allow_live_comments: false,
-        selftext_html: null,
-        likes: null,
-        suggested_sort: 'top',
-        banned_at_utc: null,
-        url_overridden_by_dest: 'https://i.redd.it/fioh7purvjn81.jpg',
-        view_count: null,
-        archived: false,
-        no_follow: false,
-        is_crosspostable: false,
-        pinned: false,
-        over_18: false,
-        preview: [Object],
-        all_awardings: [Array],
-        awarders: [],
-        media_only: false,
-        can_gild: false,
-        spoiler: false,
-        locked: false,
-        author_flair_text: null,
-        treatment_tags: [],
-        visited: false,
-        removed_by: null,
-        num_reports: null,
-        distinguished: null,
-        subreddit_id: 't5_2srr9',
-        author_is_blocked: false,
-        mod_reason_by: null,
-        removal_reason: null,
-        link_flair_background_color: '',
-        id: 'tep5ud',
-        is_robot_indexable: true,
-        report_reasons: null,
-        author: 'No-Possibility-8258',
-        discussion_type: null,
-        num_comments: 206,
-        send_replies: true,
-        whitelist_status: 'no_ads',
-        contest_mode: false,
-        mod_reports: [],
-        author_patreon_flair: false,
-        author_flair_text_color: null,
-        permalink: '/r/greentext/comments/tep5ud/anon_is_a_rapist/',
-        parent_whitelist_status: 'no_ads',
-        stickied: false,
-        url: 'https://i.redd.it/fioh7purvjn81.jpg',
-        subreddit_subscribers: 1306315,
-        created_utc: 1647351356,
-        num_crossposts: 0,
-        media: null,
-        is_video: false
-      }
-    ));
-    expect(actual.posts).toEqual(
-      {
-        approved_at_utc: null,
-        subreddit: 'greentext',
-        selftext: '',
-        author_fullname: 't2_edyu9t3r',
-        saved: false,
-        mod_reason_title: null,
-        gilded: 0,
-        clicked: false,
-        title: 'Anon is a rapist',
-        link_flair_richtext: [],
-        subreddit_name_prefixed: 'r/greentext',
-        hidden: false,
-        pwls: 0,
-        link_flair_css_class: null,
-        downs: 0,
-        thumbnail_height: 140,
-        top_awarded_type: null,
-        hide_score: false,
-        name: 't3_tep5ud',
-        quarantine: false,
-        link_flair_text_color: 'dark',
-        upvote_ratio: 0.95,
-        author_flair_background_color: null,
-        subreddit_type: 'public',
-        ups: 18578,
-        total_awards_received: 4,
-        media_embed: {},
-        thumbnail_width: 140,
-        author_flair_template_id: null,
-        is_original_content: false,
-        user_reports: [],
-        secure_media: null,
-        is_reddit_media_domain: true,
-        is_meta: false,
-        category: null,
-        secure_media_embed: {},
-        link_flair_text: null,
-        can_mod_post: false,
-        score: 18578,
-        approved_by: null,
-        is_created_from_ads_ui: false,
-        author_premium: false,
-        thumbnail: 'https://b.thumbs.redditmedia.com/WuafJXjS4QF29kz7W2yelt380P1qs8SegVzmYUPd8hM.jpg',
-        edited: false,
-        author_flair_css_class: null,
-        author_flair_richtext: [],
-        gildings: [Object],
-        post_hint: 'image',
-        content_categories: null,
-        is_self: false,
-        mod_note: null,
-        created: 1647351356,
-        link_flair_type: 'text',
-        wls: 0,
-        removed_by_category: null,
-        banned_by: null,
-        author_flair_type: 'text',
-        domain: 'i.redd.it',
-        allow_live_comments: false,
-        selftext_html: null,
-        likes: null,
-        suggested_sort: 'top',
-        banned_at_utc: null,
-        url_overridden_by_dest: 'https://i.redd.it/fioh7purvjn81.jpg',
-        view_count: null,
-        archived: false,
-        no_follow: false,
-        is_crosspostable: false,
-        pinned: false,
-        over_18: false,
-        preview: [Object],
-        all_awardings: [Array],
-        awarders: [],
-        media_only: false,
-        can_gild: false,
-        spoiler: false,
-        locked: false,
-        author_flair_text: null,
-        treatment_tags: [],
-        visited: false,
-        removed_by: null,
-        num_reports: null,
-        distinguished: null,
-        subreddit_id: 't5_2srr9',
-        author_is_blocked: false,
-        mod_reason_by: null,
-        removal_reason: null,
-        link_flair_background_color: '',
-        id: 'tep5ud',
-        is_robot_indexable: true,
-        report_reasons: null,
-        author: 'No-Possibility-8258',
-        discussion_type: null,
-        num_comments: 206,
-        send_replies: true,
-        whitelist_status: 'no_ads',
-        contest_mode: false,
-        mod_reports: [],
-        author_patreon_flair: false,
-        author_flair_text_color: null,
-        permalink: '/r/greentext/comments/tep5ud/anon_is_a_rapist/',
-        parent_whitelist_status: 'no_ads',
-        stickied: false,
-        url: 'https://i.redd.it/fioh7purvjn81.jpg',
-        subreddit_subscribers: 1306315,
-        created_utc: 1647351356,
-        num_crossposts: 0,
-        media: null,
-        is_video: false
-      }
-    );
-  });
-
-  test('should handle sortPostsAsync in progress', () => {
-    const actual = sortPostsReducer(initialState, sortPostsAsync.pending);
+  test('should handle fetchPosts in progress', () => {
+    const actual = postsReducer(initialState, fetchPosts.pending);
     expect(actual.status).toEqual('loading posts');
   });
 
   test('should handle sortPostsAsync fulfilled', () => {
-    const actual = sortPostsReducer(initialState, sortPostsAsync.fulfilled);
+    const actual = postsReducer(initialState, fetchPosts.fulfilled);
     expect(actual.status).toEqual('succeeded');
   });
 
   test('should handle sortPostsAsync failed', () => {
-    const actual = sortPostsReducer(initialState, sortPostsAsync.rejected);
+    const actual = postsReducer(initialState, fetchPosts.rejected);
     expect(actual.status).toEqual('failed to retrieve posts');
   });
 
