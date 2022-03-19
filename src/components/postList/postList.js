2ad8fa66c7b16e628ffcdee51bd3d3836cfcec88
drattylight <mlaux6@outlook.com>
2022-03-19 13:02:52 -0500

    Filter posts via search bar content

--- src/components/postList/postList.js
+++ src/components/postList/postList.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectPosts, selectStatus, fetchPosts } from '../../features/posts/postsSlice';
+import { selectPosts, selectStatus, fetchPosts, selectSearchContent } from '../../features/posts/postsSlice';
 import Post from '../post/post';
 
 function PostList() {
   const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
   const status = useSelector(selectStatus);
+  const searchContent = useSelector(selectSearchContent);
   const [postsContent, setPostsContent] = useState([]);
 
   useEffect(() => {
@@ -26,6 +27,17 @@ function PostList() {
       setPostsContent(status);
     }
   }, [status]);
+
+  useEffect(() => {
+    const filteredPosts = posts.filter(post => post.data.title.includes(searchContent));
+    const filteredPostsContent = filteredPosts.map((post) => (
+      <Post
+        key={post.data.id}
+        postData={post.data}
+      />
+    ));
+    setPostsContent(filteredPostsContent);
+  }, [searchContent]);
 
   return(
     <div>
