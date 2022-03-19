591ace3a414ffbb4069356ebf39c3bb1a783012f
drattylight <mlaux6@outlook.com>
2022-03-19 13:01:44 -0500

    Update store with search bar input

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { changeSortCategory, removePosts, fetchPosts } from '../../features/posts/postsSlice';
+import { changeSortCategory, removePosts, fetchPosts, updateSearch } from '../../features/posts/postsSlice';
 import { useDispatch } from 'react-redux';
 
 function NavBar() {
   const dispatch = useDispatch();
 
-  const onClickHome = () => {
+  const handleClickHome = () => {
     dispatch(changeSortCategory('hot'));
     dispatch(removePosts);
     dispatch(fetchPosts('hot'));
+  };
+
+  const handleSearchChange = (e) => {
+    dispatch(updateSearch(e.target.value));
   };
   
   return(
@@ -17,10 +21,17 @@ function NavBar() {
       <img src='../../media/reddit-logo.png' alt='Reddit Logo'></img>
       <h1>Mock Reddit</h1>
       <nav>
-        <NavLink to='/' onClick={onClickHome}>Home</NavLink>
+        <NavLink to='/' onClick={handleClickHome}>Home</NavLink>
       </nav>
       <form>
-        <input type='search' id='search' name='search' placeholder='Search Reddit' maxLength={50} ></input>
+        <input 
+          type='search' 
+          id='search' 
+          name='search' 
+          placeholder='Search Reddit' 
+          maxLength={50}
+          onChange={handleSearchChange} >
+        </input>
       </form>
     </header>
   );
