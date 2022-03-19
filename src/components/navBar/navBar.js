8be23bc1a13a25c3bd91fe1f63d5ec59b1680791
drattylight <mlaux6@outlook.com>
2022-03-19 11:09:05 -0500

    Add home link functionality

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { changeSortCategory, removePosts, fetchPosts } from '../../features/posts/postsSlice';
+import { useDispatch } from 'react-redux';
 
 function NavBar() {
+  const dispatch = useDispatch();
+
+  const onClickHome = () => {
+    dispatch(changeSortCategory('hot'));
+    dispatch(removePosts);
+    dispatch(fetchPosts('hot'));
+  };
+  
   return(
     <header>
       <img src='../../media/reddit-logo.png' alt='Reddit Logo'></img>
       <h1>Mock Reddit</h1>
       <nav>
-        <NavLink to='home'>Home</NavLink>
+        <NavLink to='/' onClick={onClickHome}>Home</NavLink>
       </nav>
       <form>
         <input type='search' id='search' name='search' placeholder='Search Reddit' maxLength={50} ></input>
