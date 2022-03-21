d2e9767c088addf6795e5f7b76872c1f27ce7a09
drattylight <mlaux6@outlook.com>
2022-03-21 17:01:44 -0500

    Create separate custom renders. One with router and provider. One with just provider.

--- src/test-utils/testing-library-utils.js
+++ src/test-utils/testing-library-utils.js
@@ -6,7 +6,7 @@ import postsReducer from '../features/posts/postsSlice';
 import commentsReducer from '../features/comments/commentsSlice';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function render(
+function renderProviderAndRouter(
   ui,
   {
     preloadedState,
@@ -29,7 +29,26 @@ function render(
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
+function renderProvider(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { posts: postsReducer, comments: commentsReducer }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  // eslint-disable-next-line react/prop-types
+  function Wrapper({ children }) {
+    return (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
 //re-export everything
 export * from '@testing-library/react';
 // override render method
-export { render };
+export { renderProviderAndRouter, renderProvider };
