4acd081de827642a52d4efab55209f349d214735
drattylight <mlaux6@outlook.com>
2022-03-21 17:41:27 -0500

    Add note and blank test to stop error messages

--- src/components/commentList/commentList.test.js
+++ src/components/commentList/commentList.test.js
@@ -1,2 +1,10 @@
-// Tested via integration test in app.test.js as commentList relies on input from HomePageContainer
-// to render
+import React from 'react';
+
+/*
+Tested via integration test in app.test.js as commentList relies on input from HomePageContainer
+to render
+*/
+
+test('placeholder test to stop error messages', () => {
+  <></>;
+});
--- src/containers/DetailViewContainer/DetailViewContainer.test.js
+++ src/containers/DetailViewContainer/DetailViewContainer.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
 // Tested via integration test in app.test.js as DetailViewContainer relies on input from HomePageContainer
 // to render
 
+test('placeholder test to stop error messages', () => {
+  <></>;
+});
