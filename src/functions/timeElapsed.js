121061689e1ba7bfe1640e8bea98a8fe521ab2b0
drattylight <mlaux6@outlook.com>
2022-03-22 11:03:11 -0500

    Test and code condition to only accept integers

--- src/functions/timeElapsed.js
+++ src/functions/timeElapsed.js
@@ -4,18 +4,22 @@ that has passed since the post was uploaded in hours
 */
 
 export const timeElapsed = (postTimeEpoch) => {
-  // Creates new date and sets it to Epoch of 0 to be converted to UTC seconds
-  let postDate = new Date(0);
-  postDate.setUTCSeconds(postTimeEpoch);
-  let currentDate = new Date();
-  let timeElapsed = Math.ceil((currentDate - postDate) / (1000 * 60 * 60));
+  if (postTimeEpoch % 1 === 0) {
+    // Creates new date and sets it to Epoch of 0 to be converted to UTC seconds
+    let postDate = new Date(0);
+    postDate.setUTCSeconds(postTimeEpoch);
+    let currentDate = new Date();
+    let timeElapsed = Math.ceil((currentDate - postDate) / (1000 * 60 * 60));
 
-  if (timeElapsed < 24) {
-    return `${timeElapsed} hours ago`;
-  } else if (timeElapsed >= 24 && timeElapsed < 48) {
-    return '1 day ago';
+    if (timeElapsed < 24) {
+      return `${timeElapsed} hours ago`;
+    } else if (timeElapsed >= 24 && timeElapsed < 48) {
+      return '1 day ago';
+    } else {
+      timeElapsed = Math.floor(timeElapsed / 24);
+      return `${timeElapsed} days ago`;
+    }
   } else {
-    timeElapsed = Math.floor(timeElapsed / 24);
-    return `${timeElapsed} days ago`;
+    return 'Post time failed to load';
   }
 };
