d864060a471c611387b20cc9f4a3ac3734159f1f
drattylight <mlaux6@outlook.com>
2022-03-22 17:50:05 -0500

    Add check for post media. Refactor for styling.

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { timeElapsed } from '../../functions/timeElapsed';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { fetchComments } from '../../features/comments/commentsSlice';
 import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 /*
 Creates one Reddit post. Data received from postsList component via prop
@@ -13,33 +15,59 @@ Creates one Reddit post. Data received from postsList component via prop
 function Post({ postData }) {
   const location = useLocation();
   const dispatch = useDispatch();
+  const [postMedia, setPostMedia] = useState(null);
+  const mediaFiles = [
+    '.gif',
+    '.mp4',
+    '.mov',
+    '.jpg',
+    '.png'
+  ];
 
   // Begin fetch process for comments from Reddit API while detail view of post renders
   const handleClickPost = () => {
     dispatch(fetchComments(postData.permalink));
   };
 
+  useEffect(() => {
+    mediaFiles.some(fileType => {
+      if (postData.url.includes(fileType)) {
+        setPostMedia(postData.url);
+      }
+    });
+  }, [location]);
+
+
   return (
-    <article>
-      <p>{postData.ups ? postData.ups : 'Error'}</p>
-      <p>{postData.subreddit_name_prefixed ? postData.subreddit_name_prefixed : 'Post subreddit failed to load'}</p>
-      <p>Posted by {postData.name ? postData.name : 'Post author failed to load'}</p>
-      <p>{postData.created_utc ? timeElapsed(postData.created_utc) : 'Post time failed to load'}</p>
-        
+    <article className='post'>
+      <p className='postAuthor'>
+        <span className='subredditName'>{postData.subreddit_name_prefixed ? postData.subreddit_name_prefixed + ' ' : 'Post subreddit failed to load'}</span>
+        <span>Posted by {postData.name ? postData.name : 'Post author failed to load'}</span>
+        <span>{postData.created_utc ? timeElapsed(postData.created_utc) : 'Post time failed to load'}</span>
+      </p>
       {/* Render as link if on main page but render as heading if on detail view */}
-      {location.pathname === '/detailView' ? (
-        <h2>{postData.title ? postData.title : 'Post title failed to load'}</h2>
-      ) : (
-        <NavLink
-          to="/detailView"
-          state={{ postData: postData }}
-          onClick={handleClickPost}
-        >
-          {postData.title ? postData.title : 'Post title failed to load'}
-        </NavLink>
-      )}
-      {postData.url ? <img src={postData.url} alt={postData.url}></img> : 'Post picture failed to load'}
-      <p>{postData.num_comments ? postData.num_comments : 'Number of comments failed to load'} comments</p>
+      <div className='postTitle'>
+        {location.pathname === '/detailView' ? (
+          <h2>{postData.title ? postData.title : 'Post title failed to load'}</h2>
+        ) : (
+          <NavLink
+            to="/detailView"
+            state={{ postData: postData }}
+            onClick={handleClickPost}
+          >
+            {postData.title ? postData.title : 'Post title failed to load'}
+          </NavLink>
+        )}
+      </div>
+      <p>{postData.selftext}</p>
+      {/*postMedia ? <img src={postMedia} alt={postMedia}></img> : 'Post picture failed to load'*/}
+      {postMedia !== null && (<img src={postMedia} alt={postMedia} className='postImg'></img>)}
+      <section className='postUpvotes'>
+        <FontAwesomeIcon icon={solid('arrow-up')} className='upArrow' />
+        <p>{postData.ups ? postData.ups : 'Error'}</p>
+        <FontAwesomeIcon icon={solid('arrow-down')} className='downArrow' />
+      </section>
+      <p className='numComments'>{postData.num_comments ? postData.num_comments : 'Number of comments failed to load'} comments</p>
     </article>
   );
 }
--- src/components/post/post.test.js
+++ src/components/post/post.test.js
@@ -6,12 +6,12 @@ import {
 import Post from './post';
 
 describe('<Post />', () => {
-  test('renders correct subreddit, user, title, pic, votes, # of comments', () => {
+  test('renders correct subreddit, user, title, pic, selftext, votes, # of comments', () => {
     // mock postData
     const postData = {
       approved_at_utc: null,
       subreddit: 'greentext',
-      selftext: '',
+      selftext: 'Sample self text',
       author_fullname: 't2_edyu9t3r',
       saved: false,
       mod_reason_title: null,
@@ -128,6 +128,7 @@ describe('<Post />', () => {
     const PostTitle = screen.getByRole('link', { name: /anon is a rapist/i });
     const PostSubreddit = screen.getByText(/r\/greentext/i);
     const PostUser = screen.getByText(/t3_tep5ud/i);
+    const PostSelfText = screen.getByText(/sample self text/i);
     const Pic = screen.getByRole('img', {
       name: 'https://i.redd.it/fioh7purvjn81.jpg',
     });
@@ -137,6 +138,7 @@ describe('<Post />', () => {
     expect(PostTitle).toBeInTheDocument();
     expect(PostSubreddit).toBeInTheDocument();
     expect(PostUser).toBeInTheDocument();
+    expect(PostSelfText).toBeInTheDocument();
     expect(Pic).toBeInTheDocument();
     expect(PostUpvotes).toBeInTheDocument();
     expect(PostComments).toBeInTheDocument();
@@ -249,7 +251,7 @@ describe('<Post />', () => {
       permalink: '/r/greentext/comments/tep5ud/anon_is_a_rapist/',
       parent_whitelist_status: 'no_ads',
       stickied: false,
-      url: null,
+      url: 'placeholderURL',
       subreddit_subscribers: 1306315,
       created_utc: null,
       num_crossposts: 0,
@@ -264,7 +266,6 @@ describe('<Post />', () => {
     const postTime = screen.getByText(/post time failed to load/i);
     const postUpvotes = screen.getByText(/error/i);
     const postSubreddit = screen.getByText(/post subreddit failed to load/i);
-    const postPic = screen.getByText(/post picture failed to load/i);
     const postNumComments = screen.getByText(/number of comments failed to load/i);
 
     expect(postTitle).toBeInTheDocument();
@@ -272,8 +273,128 @@ describe('<Post />', () => {
     expect(postTime).toBeInTheDocument();
     expect(postUpvotes).toBeInTheDocument();
     expect(postSubreddit).toBeInTheDocument();
-    expect(postPic).toBeInTheDocument();
     expect(postNumComments).toBeInTheDocument();
+  });
+
+  test('image element does not load if post data does not have image', () => {
+    // mock postData with Image removed
+    const postData = {
+      approved_at_utc: null,
+      subreddit: 'greentext',
+      selftext: 'Sample self text',
+      author_fullname: 't2_edyu9t3r',
+      saved: false,
+      mod_reason_title: null,
+      gilded: 0,
+      clicked: false,
+      title: 'Anon is a rapist',
+      link_flair_richtext: [],
+      subreddit_name_prefixed: 'r/greentext',
+      hidden: false,
+      pwls: 0,
+      link_flair_css_class: null,
+      downs: 0,
+      thumbnail_height: 140,
+      top_awarded_type: null,
+      hide_score: false,
+      name: 't3_tep5ud',
+      quarantine: false,
+      link_flair_text_color: 'dark',
+      upvote_ratio: 0.95,
+      author_flair_background_color: null,
+      subreddit_type: 'public',
+      ups: 18578,
+      total_awards_received: 4,
+      media_embed: {},
+      thumbnail_width: 140,
+      author_flair_template_id: null,
+      is_original_content: false,
+      user_reports: [],
+      secure_media: null,
+      is_reddit_media_domain: true,
+      is_meta: false,
+      category: null,
+      secure_media_embed: {},
+      link_flair_text: null,
+      can_mod_post: false,
+      score: 18578,
+      approved_by: null,
+      is_created_from_ads_ui: false,
+      author_premium: false,
+      thumbnail:
+        'https://b.thumbs.redditmedia.com/WuafJXjS4QF29kz7W2yelt380P1qs8SegVzmYUPd8hM.jpg',
+      edited: false,
+      author_flair_css_class: null,
+      author_flair_richtext: [],
+      gildings: [Object],
+      post_hint: 'image',
+      content_categories: null,
+      is_self: false,
+      mod_note: null,
+      created: 1647351356,
+      link_flair_type: 'text',
+      wls: 0,
+      removed_by_category: null,
+      banned_by: null,
+      author_flair_type: 'text',
+      domain: 'i.redd.it',
+      allow_live_comments: false,
+      selftext_html: null,
+      likes: null,
+      suggested_sort: 'top',
+      banned_at_utc: null,
+      url_overridden_by_dest: 'https://i.redd.it/fioh7purvjn81.jpg',
+      view_count: null,
+      archived: false,
+      no_follow: false,
+      is_crosspostable: false,
+      pinned: false,
+      over_18: false,
+      preview: [Object],
+      all_awardings: [Array],
+      awarders: [],
+      media_only: false,
+      can_gild: false,
+      spoiler: false,
+      locked: false,
+      author_flair_text: null,
+      treatment_tags: [],
+      visited: false,
+      removed_by: null,
+      num_reports: null,
+      distinguished: null,
+      subreddit_id: 't5_2srr9',
+      author_is_blocked: false,
+      mod_reason_by: null,
+      removal_reason: null,
+      link_flair_background_color: '',
+      id: 'tep5ud',
+      is_robot_indexable: true,
+      report_reasons: null,
+      author: 'No-Possibility-8258',
+      discussion_type: null,
+      num_comments: 206,
+      send_replies: true,
+      whitelist_status: 'no_ads',
+      contest_mode: false,
+      mod_reports: [],
+      author_patreon_flair: false,
+      author_flair_text_color: null,
+      permalink: '/r/greentext/comments/tep5ud/anon_is_a_rapist/',
+      parent_whitelist_status: 'no_ads',
+      stickied: false,
+      url: 'https://www.reddit.com/r/OnePiece/comments/tk0mlk/chapter_1044_spoilers/',
+      subreddit_subscribers: 1306315,
+      created_utc: 1647351356,
+      num_crossposts: 0,
+      media: null,
+      is_video: false,
+    };
+
+    render(<Post postData={postData} />);
+
+    const imageElement = screen.queryByRole('img');
+    expect(imageElement).not.toBeInTheDocument();
   });
 
 });
